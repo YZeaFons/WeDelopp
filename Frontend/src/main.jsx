eebeb48c6013b7c1cc141ef004185cd70b442073
YZeaFons <yeisonyamidzea@gmail.com>
2024-02-29 05:49:21 -0500

    previo a entrega

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -34,7 +34,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Auth0Provider
         domain="dev-zy68xm5sl5mhrgw3.us.auth0.com"
-        clientId="rH6qLmDrP51hwYBROC1JUlmZy5HbPkwl"
+        clientId="OuHXebWJlbgAdtqZFWNQSBzNv1veHirJ"
         authorizationParams={{
           redirect_uri: window.location.origin,
         }}
